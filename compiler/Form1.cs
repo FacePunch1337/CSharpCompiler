e5a9df4210596eac62c42e4e49388315c7eee073
Stas Solomon <Solo_jf68@student.itstep.org>
2022-06-17 10:51:09 +0300

    Добавьте файлы проекта.

---
+++ compiler/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using Microsoft.CSharp;
+
+namespace compiler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CSharpCodeProvider provider = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", tbFrameworke.Text } });
+
+            CompilerParameters parameters = new CompilerParameters(new string[] { "mscorlib.dll", "System.Core.dll" }, tbFilePath.Text, true);
+
+            parameters.GenerateExecutable = true;
+
+            CompilerResults results = provider.CompileAssemblyFromSource(parameters, tbCodeArea.Text);
+            
+            if (results.Errors.HasErrors)
+            {
+                foreach(CompilerError error in results.Errors.Cast<CompilerError>())
+                {
+                    tbDebugArea.Text += $"Строка {error.Line}: {error.ErrorText}";
+                }
+                
+            }
+            else
+            {
+                tbDebugArea.Text = "[Сборка завершена]";
+
+                Process.Start($"{Application.StartupPath}/{tbFilePath.Text}");
+            }
+        }
+
+    }
+}  
